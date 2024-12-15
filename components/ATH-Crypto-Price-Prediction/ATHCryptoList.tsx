135c95641dd56770167a0ee6b87d8d86a49bbc06
SolomonFoskaay <oskfak@gmail.com>
2024-12-15 12:18:54 +0000

    Fixes 30 for FoskaayFib Crypto ATH Prediction Tool

--- components/ATH-Crypto-Price-Prediction/ATHCryptoList.tsx
+++ components/ATH-Crypto-Price-Prediction/ATHCryptoList.tsx
@@ -2,8 +2,8 @@
 export const cryptoSymbols = [
     'BTC', 'ETH', 'XRP', 'SOL', 'BNB', 'DOGE', 'ADA', 'TRX', 'AVAX', 'SHIB', 
     'TON', 'LINK', 'DOT', 'XLM', 'BCH', 'SUI', 'PEPE', 'HBAR', 'UNI', 'SUPER', 
-    'LTC', 'NEAR', 'APT', 'ICP', 'MATIC', 'ETC', 'CRO', 'RENDER', 'TAO', 'VET', 
-    'KAS', 'FET', 'ARB', 'AAVE', 'MINA', 'FIL', 'BGB', 'ALGO', 'OM', 'ATOM', 
+    'LTC', 'NEAR', 'APT', 'ICP', 'MATIC', 'ETC', 'CRO', 'AXL', 'TAO', 'VET', 
+    'KAS', 'FET', 'RON', 'AAVE', 'MINA', 'FIL', 'BGB', 'ALGO', 'OM', 'ATOM', 
     'OKB', 'STX', 'XMR', 'FTM', 'WIF', 'BONK', "TIA", 'IMX', 'OP', 'ENA', 
     'INJ', 'GRT', 'FLOKI', 'THETA', 'SEI', 'WLD', 'RUNE', 'ONDO', 'JASMY', 'GALA', 
     'SAND', 'MKR', 'LDO', 'EOS', 'QNT', 'BRETT', 'BEAM', 'KAIA', 'KCS', 'PYTH', 
@@ -16,8 +16,8 @@ export const cryptoSymbols = [
   export const cryptoNames = [
     'Bitcoin', 'Ethereum', 'Ripple', 'Solana', 'Binance', 'Dogecoin', 'Cardano', 'Tron', 'Avalanche', 'Shiba Inu', 
     'Toncoin', 'Chainlink', 'Polkadot', 'Stellar Lumen', 'Bitcoin Cash', 'Sui', 'Pepe', 'Hedera', 'Uniswap', 'Super', 
-    'Litecoin', 'Near Protocol', 'Aptos', 'Internet Computer', 'Polygon (POL ex-MATIC)', 'Ethereum Classic', 'Cronos', 'Render', 'Bittensor', 'VeChain', 
-    'Kaspa', 'Artificial SuperInterlligience Alliance', 'Arbitrum', 'Aave', 'Mina', 'Filecoin', 'Bitget Token', 'Algorand', 'Mantra', 'Cosmos', 
+    'Litecoin', 'Near Protocol', 'Aptos', 'Internet Computer', 'Polygon (POL ex-MATIC)', 'Ethereum Classic', 'Cronos', 'Axelar', 'Bittensor', 'VeChain', 
+    'Kaspa', 'Artificial SuperInterlligience Alliance', 'Ronin', 'Aave', 'Mina', 'Filecoin', 'Bitget Token', 'Algorand', 'Mantra', 'Cosmos', 
     'OKB', 'Stacks', 'Monero', 'Fantom', 'DogWifHat', 'Bonk', "Celestia", 'Immutable', 'Optimism', 'Ethena', 
     'Injective', 'The Graph', 'Floki', 'Theta Network', 'Sei', 'Worldcoin', 'Thorchain', 'Ondo', 'JasmyCoin', 'GalaChain', 
     'The Sandbox', 'Maker', 'Beam', 'EOS', 'Quant', 'Brett (Based)', 'Beam', 'Kaia', 'KuCoin Token', 'Pyth Network', 
